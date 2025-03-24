9a6f296386a6adaae3b6fe507123cc00a10957c5
panda-gallery <3997946646@qq.com>
2025-03-24 23:54:18 +0800

    rust_web_learn

---
+++ src/store.rs
@@ -0,0 +1,155 @@
+use sqlx::postgres::{PgPool, PgPoolOptions, PgRow};
+use sqlx::Row;
+
+use handle_errors::Error;
+
+use crate::types::{
+    answer::{Answer, AnswerId, NewAnswer},
+    question::{NewQuestion, Question, QuestionId},
+};
+
+#[derive(Debug, Clone)]
+pub struct Store {
+    pub connection: PgPool,
+}
+
+impl Store {
+    pub async fn new(db_url: &str) -> Self {
+        let db_pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(db_url)
+            .await
+            .expect("❌ 数据库连接失败，请检查：\n1. 服务是否运行\n2. 连接字符串格式\n3. 用户名密码");
+
+        Store {
+            connection: db_pool,
+        }
+    }
+
+    pub async fn get_questions(
+        &self,
+        limit: Option<i32>,
+        offset: i32,
+    ) -> Result<Vec<Question>, Error> {
+        match sqlx::query("SELECT * from questions LIMIT $1 OFFSET $2")
+            .bind(limit)
+            .bind(offset)
+            .map(|row: PgRow| Question {
+                id: QuestionId(row.get("id")),
+                title: row.get("title"),
+                content: row.get("content"),
+                tags: row.get("tags"),
+            })
+            .fetch_all(&self.connection)
+            .await
+        {
+            Ok(questions) => Ok(questions),
+            Err(e) => {
+                tracing::error!("{:?}", e);
+                Err(Error::DatabaseQueryError)
+            }
+        }
+    }
+
+    pub async fn add_question(
+        &self,
+        new_question: NewQuestion,
+    ) -> Result<Question, Error> {
+        match sqlx::query(
+            "INSERT INTO questions (title, content, tags)
+                 VALUES ($1, $2, $3)
+                 RETURNING id, title, content, tags",
+        )
+        .bind(new_question.title)
+        .bind(new_question.content)
+        .bind(new_question.tags)
+        .map(|row: PgRow| Question {
+            id: QuestionId(row.get("id")),
+            title: row.get("title"),
+            content: row.get("content"),
+            tags: row.get("tags"),
+        })
+        .fetch_one(&self.connection)
+        .await
+        {
+            Ok(question) => Ok(question),
+            Err(e) => {
+                tracing::error!( "{:?}", e);
+                Err(Error::DatabaseQueryError)
+            }
+        }
+    }
+
+    pub async fn update_question(
+        &self,
+        question: Question,
+        question_id: i32,
+    ) -> Result<Question, Error> {
+        match sqlx::query(
+            "UPDATE questions SET title = $1, content = $2, tags = $3
+        WHERE id = $4
+        RETURNING id, title, content, tags",
+        )
+        .bind(question.title)
+        .bind(question.content)
+        .bind(question.tags)
+        .bind(question_id)
+        .map(|row: PgRow| Question {
+            id: QuestionId(row.get("id")),
+            title: row.get("title"),
+            content: row.get("content"),
+            tags: row.get("tags"),
+        })
+        .fetch_one(&self.connection)
+        .await
+        {
+            Ok(question) => Ok(question),
+            Err(e) => {
+                tracing::error!("{:?}", e);
+                Err(Error::DatabaseQueryError)
+            }
+        }
+    }
+
+    pub async fn delete_question(
+        &self,
+        question_id: i32,
+    ) -> Result<bool, Error> {
+        match sqlx::query("DELETE FROM questions WHERE id = $1")
+            .bind(question_id)
+            .execute(&self.connection)
+            .await
+        {
+            Ok(_) => Ok(true),
+            Err(e) => {
+                tracing::error!("{:?}", e);
+                Err(Error::DatabaseQueryError)
+            }
+        }
+    }
+
+    pub async fn add_answer(
+        &self,
+        new_answer: NewAnswer,
+    ) -> Result<Answer, Error> {
+        match sqlx::query(
+            "INSERT INTO answers (content, question_id) VALUES ($1, $2)",
+        )
+        .bind(new_answer.content)
+        .bind(new_answer.question_id.0)
+        .map(|row: PgRow| Answer {
+            id: AnswerId(row.get("id")),
+            content: row.get("content"),
+            question_id: QuestionId(row.get("question_id")),
+        })
+        .fetch_one(&self.connection)
+        .await
+        {
+            Ok(answer) => Ok(answer),
+            Err(e) => {
+                tracing::error!( "{:?}", e);
+                Err(Error::DatabaseQueryError)
+            }
+        }
+    }
+}
---
+++ src/types/pagination.rs
@@ -0,0 +1,48 @@
+use handle_errors::Error;
+use std::collections::HashMap;
+
+/// Pagination struct which is getting extract
+/// from query params
+#[derive(Debug, Default)]
+pub struct Pagination {
+    pub limit: Option<i32>,
+    pub offset: i32,
+}
+
+/// Extract query parameters from the `/questions` route
+/// # Example query
+/// GET requests to this route can have a pagination attached so we just
+/// return the questions we need
+/// `/questions?start=1&end=10`
+/// # Example usage
+/// ```rust
+/// use std::collections::HashMap;
+///
+/// let mut query = HashMap::new();
+/// query.insert("start".to_string(), "1".to_string());
+/// query.insert("end".to_string(), "10".to_string());
+/// let p = pagination::extract_pagination(query).unwrap();
+/// assert_eq!(p.start, 1);
+/// assert_eq!(p.end, 10);
+/// ```
+// 解析参数 -- 分页
+pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
+    if params.contains_key("limit") && params.contains_key("offset") {
+        return Ok(Pagination {
+            limit: Some(
+                params
+                    .get("limit")
+                    .unwrap()
+                    .parse::<i32>()
+                    .map_err(Error::ParseError)?,
+            ),
+
+            offset: params
+                .get("offset")
+                .unwrap()
+                .parse::<i32>()
+                .map_err(Error::ParseError)?,
+        });
+    }
+    Err(Error::MissingParameters)
+}
